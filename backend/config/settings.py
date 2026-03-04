6c48f2d4f69d1620199536fcf389a8013b12d2ad
vijay <pnvijay250@gmail.com>
2026-03-04 13:47:39 +0530

    fix secure headers and media proxy

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -32,6 +32,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+# Security Settings for Secure Cookies & HTTPS
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 
 # Application definition
 
