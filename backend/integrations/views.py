62ade662dbce2f84cd0f57e05d12f99aa9277b92
intasolutionpvtltd@gmail.com <intasolutionpvtltd@gmail.com>
2026-05-22 21:02:27 +0530

    Debug classroom object completely

--- backend/integrations/views.py
+++ backend/integrations/views.py
@@ -72,14 +72,7 @@ class LMSProxyView(views.APIView):
                 for cs in class_summary_list:
                     # Extract course name from nested classroom object or root object
                     classroom = cs.get('classroom', {})
-                    course_name = ""
-                    if isinstance(classroom, dict):
-                        course_name = classroom.get('name') or classroom.get('title') or classroom.get('subject') or classroom.get('className')
-                    elif isinstance(classroom, str):
-                        course_name = classroom
-                        
-                    if not course_name:
-                        course_name = cs.get('className') or cs.get('title') or cs.get('subject') or cs.get('name') or 'Unknown Course'
+                    course_name = str(classroom)
                         
                     enrolled_courses.append({
                         "id": cs.get('classId'),
