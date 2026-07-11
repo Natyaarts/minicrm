e4b22c5f8e4c188f45e7080f418a44a209eaed7e
intasolutionpvtltd@gmail.com <intasolutionpvtltd@gmail.com>
2026-07-11 11:11:00 +0530

    Use pre-signed URLs for S3 for security and fix image loading

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -216,7 +216,9 @@ if os.getenv('USE_AWS_S3') == 'True':
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'us-east-1')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    AWS_DEFAULT_ACL = 'public-read'
+    AWS_DEFAULT_ACL = 'private'
+    AWS_QUERYSTRING_AUTH = True
+    AWS_S3_FILE_OVERWRITE = False
     
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
