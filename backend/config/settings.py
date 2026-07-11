d6e252f6abfe7ee153c91c082e46c70802282eee
intasolutionpvtltd@gmail.com <intasolutionpvtltd@gmail.com>
2026-07-11 11:21:42 +0530

    Remove custom domain so S3 presigned URLs work properly

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -215,13 +215,11 @@ if os.getenv('USE_AWS_S3') == 'True':
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'us-east-1')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_DEFAULT_ACL = 'private'
     AWS_QUERYSTRING_AUTH = True
     AWS_S3_FILE_OVERWRITE = False
     
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 else:
     MEDIA_URL = 'https://natyaarts.org/api/media/'
     MEDIA_ROOT = BASE_DIR / 'media'
