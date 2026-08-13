08a9bb3c9f3b895f6b8e85f4f7f87c01b8c3990a
intasolutionpvtltd@gmail.com <intasolutionpvtltd@gmail.com>
2026-08-13 13:09:47 +0530

    Wrap sheet name in single quotes

--- scratch_test.py
+++ scratch_test.py
@@ -19,11 +19,17 @@ print('Available sheets:')
 for sheet in meta_data.get('sheets', []):
     print(' - Name:', repr(sheet.get('properties', {}).get('title')))
 
-# Let's test fetch range using sheet title verbatim without quotes
+# Test quoting the sheet title in single quotes
 sheet_title = c.google_sheet_name or 'Sheet1'
-url_no_quote = f'https://sheets.googleapis.com/v4/spreadsheets/{c.google_spreadsheet_id}/values/{sheet_title}!A:Z'
-res_no_quote = requests.get(url_no_quote, headers={'Authorization': f'Bearer {token}'})
-print('No urllib quote status:', res_no_quote.status_code)
-if res_no_quote.status_code != 200:
-    print('No urllib quote response:', res_no_quote.text[:200])
+quoted_title = f"'{sheet_title}'!A:Z"
+import urllib.parse
+encoded_range = urllib.parse.quote(quoted_title)
+url_quoted = f'https://sheets.googleapis.com/v4/spreadsheets/{c.google_spreadsheet_id}/values/{encoded_range}'
+res_quoted = requests.get(url_quoted, headers={'Authorization': f'Bearer {token}'})
+print('Quoted sheet name status:', res_quoted.status_code)
+if res_quoted.status_code != 200:
+    print('Quoted response:', res_quoted.text[:200])
+else:
+    print('SUCCESS! Sample data:', res_quoted.json().get('values', [])[:2])
+
 
