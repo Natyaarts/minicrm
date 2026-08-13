c196a040d094e688c93fb6e986006546da942cab
intasolutionpvtltd@gmail.com <intasolutionpvtltd@gmail.com>
2026-08-13 13:02:17 +0530

    Add debug script

---
+++ scratch_test.py
@@ -0,0 +1,19 @@
+import sys
+sys.path.append('/home/ubuntu/minicrm/backend')
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+import django
+django.setup()
+
+from crm.models import Campaign
+from crm.views_google import get_refreshed_access_token
+import requests
+import urllib.parse
+
+c = Campaign.objects.get(id=6)
+token = get_refreshed_access_token(c)
+sheet_name_part = urllib.parse.quote(c.google_sheet_name or 'Sheet1')
+url = f'https://sheets.googleapis.com/v4/spreadsheets/{c.google_spreadsheet_id}/values/{sheet_name_part}!A:Z'
+res = requests.get(url, headers={'Authorization': f'Bearer {token}'})
+print('Status:', res.status_code)
+print('Response:', res.text)
