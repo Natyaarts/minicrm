41205ef2546602e57915b3260fc3f45ceb96d46a
intasolutionpvtltd@gmail.com <intasolutionpvtltd@gmail.com>
2026-08-13 13:04:07 +0530

    Update debug prints in scratch_test.py

--- scratch_test.py
+++ scratch_test.py
@@ -11,9 +11,11 @@ import requests
 import urllib.parse
 
 c = Campaign.objects.get(id=6)
+print('Spreadsheet ID:', repr(c.google_spreadsheet_id))
+print('Sheet Name:', repr(c.google_sheet_name))
 token = get_refreshed_access_token(c)
 sheet_name_part = urllib.parse.quote(c.google_sheet_name or 'Sheet1')
 url = f'https://sheets.googleapis.com/v4/spreadsheets/{c.google_spreadsheet_id}/values/{sheet_name_part}!A:Z'
 res = requests.get(url, headers={'Authorization': f'Bearer {token}'})
 print('Status:', res.status_code)
-print('Response:', res.text)
+print('Response:', res.text[:200])
