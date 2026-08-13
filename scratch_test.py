6a616d943fdc81be6f14c10cd3c1c5ebf7be9e68
intasolutionpvtltd@gmail.com <intasolutionpvtltd@gmail.com>
2026-08-13 13:04:50 +0530

    Fetch spreadsheet metadata to check validity

--- scratch_test.py
+++ scratch_test.py
@@ -14,6 +14,12 @@ c = Campaign.objects.get(id=6)
 print('Spreadsheet ID:', repr(c.google_spreadsheet_id))
 print('Sheet Name:', repr(c.google_sheet_name))
 token = get_refreshed_access_token(c)
+# Let's test requesting just the metadata of the spreadsheet first, to see if the spreadsheet ID is valid
+url_meta = f'https://sheets.googleapis.com/v4/spreadsheets/{c.google_spreadsheet_id}'
+res_meta = requests.get(url_meta, headers={'Authorization': f'Bearer {token}'})
+print('Meta Status:', res_meta.status_code)
+print('Meta Response:', res_meta.text[:200])
+
 sheet_name_part = urllib.parse.quote(c.google_sheet_name or 'Sheet1')
 url = f'https://sheets.googleapis.com/v4/spreadsheets/{c.google_spreadsheet_id}/values/{sheet_name_part}!A:Z'
 res = requests.get(url, headers={'Authorization': f'Bearer {token}'})
