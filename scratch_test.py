c0cfb0a8b6ff4b917c713aaf036f1dc7244f4df2
intasolutionpvtltd@gmail.com <intasolutionpvtltd@gmail.com>
2026-08-13 13:08:34 +0530

    Fetch list of sheets and test plain range url

--- scratch_test.py
+++ scratch_test.py
@@ -11,17 +11,19 @@ import requests
 import urllib.parse
 
 c = Campaign.objects.get(id=6)
-print('Spreadsheet ID:', repr(c.google_spreadsheet_id))
-print('Sheet Name:', repr(c.google_sheet_name))
 token = get_refreshed_access_token(c)
-# Let's test requesting just the metadata of the spreadsheet first, to see if the spreadsheet ID is valid
 url_meta = f'https://sheets.googleapis.com/v4/spreadsheets/{c.google_spreadsheet_id}'
 res_meta = requests.get(url_meta, headers={'Authorization': f'Bearer {token}'})
-print('Meta Status:', res_meta.status_code)
-print('Meta Response:', res_meta.text[:200])
+meta_data = res_meta.json()
+print('Available sheets:')
+for sheet in meta_data.get('sheets', []):
+    print(' - Name:', repr(sheet.get('properties', {}).get('title')))
 
-sheet_name_part = urllib.parse.quote(c.google_sheet_name or 'Sheet1')
-url = f'https://sheets.googleapis.com/v4/spreadsheets/{c.google_spreadsheet_id}/values/{sheet_name_part}!A:Z'
-res = requests.get(url, headers={'Authorization': f'Bearer {token}'})
-print('Status:', res.status_code)
-print('Response:', res.text[:200])
+# Let's test fetch range using sheet title verbatim without quotes
+sheet_title = c.google_sheet_name or 'Sheet1'
+url_no_quote = f'https://sheets.googleapis.com/v4/spreadsheets/{c.google_spreadsheet_id}/values/{sheet_title}!A:Z'
+res_no_quote = requests.get(url_no_quote, headers={'Authorization': f'Bearer {token}'})
+print('No urllib quote status:', res_no_quote.status_code)
+if res_no_quote.status_code != 200:
+    print('No urllib quote response:', res_no_quote.text[:200])
+
